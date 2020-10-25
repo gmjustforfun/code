7551909edf61bddfeafdff223c2a3390661dc62f
carolyn_mini <13725263926@163.com>
2020-10-25 14:44:10 +0800

    demo_func:将pso相关测试函数的定义全部写入

--- sko/demo_func.py
+++ sko/demo_func.py
@@ -18,7 +18,7 @@ def function_for_TSP(num_points, seed=None):
 
     return num_points, points_coordinate, distance_matrix, cal_total_distance
 
-
+# ok
 def sphere(p):
     # Sphere函数
     out_put = 0
@@ -26,6 +26,38 @@ def sphere(p):
         out_put += i ** 2
     return out_put
 
+def schs222(x):
+    out_put = 0
+    out_put01 = 1
+    for i in x:
+        out_put += abs(i)
+        out_put01 = abs(i)*out_put01
+    out_put = out_put01+out_put
+    return out_put
+
+def Schwefel_P222(x):
+    if x.ndim == 1:
+        x = x.reshape(1, -1)
+    return np.sum(np.abs(x), axis=1) + np.prod(np.abs(x), axis=1)
+
+def schls221(x):
+    max = -np.inf
+    for xi in x:
+        if abs(xi) > max:
+            max = abs(xi)
+    return max
+
+def schwefel(x):
+    sum = 0
+    for xi in x:
+        sum += (-xi*np.sin(xi))
+    return sum
+
+def Step(x):
+    sum = 0
+    for xi in x:
+        sum = sum + np.floor(xi+0.5) ** 2
+    return sum
 
 def schaffer(p):
     '''
@@ -36,7 +68,6 @@ def schaffer(p):
     x1, x2 = p
     x = np.square(x1) + np.square(x2)
     return 0.5 + (np.square(np.sin(np.sqrt(x))) - 0.5) / np.square(1 + 0.001 * x)
-
 
 def shubert(p):
     '''
@@ -49,6 +80,35 @@ def shubert(p):
     part2 = [i * np.cos((i + 1) * y + i) for i in range(1, 6)]
     return np.sum(part1) * np.sum(part2)
 
+def quadric(x):
+
+    result = 0
+    for i in range(len(x)):
+        sum = 0
+        for j in range(i+1):
+            sum += x[j]
+        sum = sum**2
+        result += sum
+    return result
+
+def quadricNoise(x):
+    sum = 0
+    i = 0
+    for xi in x:
+        sum += i*xi**4
+        i += 1
+    result = sum + np.random.rand()
+    return result
+
+def ackley(x):
+    part1 = 0
+    part2 = 0
+    for i in x:
+        part1 += (i ** 2)
+        part2 += np.cos(2 * np.pi * i)
+    left = 20 * np.exp(-0.2 * ((part1 / x.shape[0]) ** .5))
+    right = np.exp(part2 / x.shape[0])
+    return -left - right + 20 + np.e
 
 def griewank(p):
     '''
@@ -61,7 +121,6 @@ def griewank(p):
     part2 = [np.cos(x / np.sqrt(i + 1)) for i, x in enumerate(p)]
     return np.sum(part1) - np.prod(part2) + 1
 
-
 def rastrigrin(p):
     '''
     多峰值函数，也是典型的非线性多模态函数
@@ -70,7 +129,6 @@ def rastrigrin(p):
     has a global minimum at x = 0  where f(x) = 0
     '''
     return np.sum([np.square(x) - 10 * np.cos(2 * np.pi * x) + 10 for x in p])
-
 
 def rosenbrock(p):
     '''
@@ -89,171 +147,6 @@ def rosenbrock(p):
 def Schaffers_f6_function(p):
     x,y = p
     res = 0.5+((math.sin(x**2+y**2))**2-0.5)/((1+0.001*(x**2+y**2))**2)
-
-"定义测试函数"
-def demo_func(x):
-    x1, x2, x3 = x
-    return x1 ** 2 + (x2 - 0.05) ** 2 + x3 ** 2
-def demo_func_01_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    return x1 ** 2 + x2 ** 2 + x3 ** 2+ x4 ** 2+ x5 ** 2+ x6 ** 2+ x7 ** 2+ x8 ** 2+ x9 ** 2+ x10 ** 2
-def demo_func_01_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    return x1 ** 2 + x2 ** 2 + x3 ** 2+ x4 ** 2+ x5 ** 2+ x6 ** 2+ x7 ** 2+ x8 ** 2+ x9 ** 2+ x10 ** 2+x11 ** 2+x12 ** 2+x13 ** 2+x14 ** 2+x15 ** 2+x16 ** 2+x17 ** 2+x18 ** 2+x19 ** 2+x20 ** 2+ x21 ** 2+x22 ** 2+x23 ** 2+x24 ** 2+x25 ** 2+x26 ** 2+x27 ** 2+x28 ** 2+x29 ** 2+x30 ** 2
-
-def demo_func_02_10(x):
-    x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 = x
-    return abs(x1)+ abs(x2)+ abs(x3)+ abs(x4)+ abs(x5)+ abs(x6)+ abs(x7)+ abs(x8)+ abs(x9)+ abs(x10)+abs(x1)*abs(x2)*abs(x3)*abs(x4)*abs(x5)*abs(x6)*abs(x7)*abs(x8)*abs(x9)*abs(x10)
-def demo_func_02_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    return abs(x1)+ abs(x2)+ abs(x3)+ abs(x4)+ abs(x5)+ abs(x6)+ abs(x7)+ abs(x8)+ abs(x9)+ abs(x10)+abs(x1)*abs(x2)*abs(x3)*abs(x4)*abs(x5)*abs(x6)*abs(x7)*abs(x8)*abs(x9)*abs(x10)+abs(x11)+ abs(x12)+ abs(x13)+ abs(x14)+ abs(x15)+ abs(x16)+ abs(x17)+ abs(x18)+ abs(x19)+ abs(x20)+abs(x11)*abs(x12)*abs(x13)*abs(x14)*abs(x15)*abs(x16)*abs(x17)*abs(x18)*abs(x19)*abs(x20)+abs(x21)+ abs(x22)+ abs(x23)+ abs(x24)+ abs(x25)+ abs(x26)+ abs(x27)+ abs(x28)+ abs(x29)+ abs(x30)+abs(x21)*abs(x22)*abs(x23)*abs(x24)*abs(x25)*abs(x26)*abs(x27)*abs(x28)*abs(x29)*abs(x30)
-
-def demo_func_03_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10]
-    # print(x)
-    # print(x1)
-    # print("xx------------------------------------------")
-    # print(xx)
-    sum_total=0
-    for i in range(10):
-        sum = 0
-        for j in range(i+1):
-            sum += xx[j]
-        sum = sum ** 2
-        sum_total = sum_total+sum
-    return sum_total
-def demo_func_03_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30]
-    sum_total = 0
-    for i in range(30):
-        sum = 0
-        for j in range(i + 1):
-            sum += xx[j]
-        sum = sum ** 2
-        sum_total = sum_total + sum
-    return sum_total
-def demo_func_04_10(x):
-    x1,x2,x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10]
-    xx_1=xx.copy()
-    for i in range(10):
-        xx_1[i] = abs(xx[i])
-    return max(xx_1)
-def demo_func_04_30(x):
-    x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    xx_1 = xx.copy()
-    for i in range(30):
-        xx_1[i] = abs(xx[i])
-    return max(xx_1)
-def demo_func_05_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum = 0
-    for i in range(9):
-        sum = sum + 100*((xx[i+1]-xx[i]**2)**2)+(xx[i]-1)**2
-    return  sum
-def demo_func_05_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum = 0
-    for i in range(29):
-        sum = sum + 100*((xx[i + 1] - xx[i] ** 2) ** 2) + (xx[i] - 1) ** 2
-    return sum
-def demo_func_06_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum = 0
-    for i in range(10):
-        a = xx[i] + 0.5
-        a1 = math.floor(a)
-        a1 = a1**2
-        sum = sum+a1
-    return sum
-def demo_func_06_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum = 0
-    for i in range(30):
-        a = xx[i] + 0.5
-        a1 = math.floor(a)
-        a1 = a1 ** 2
-        sum = sum + a1
-    return sum
-"注意：这里文章中的公式表述不明"
-def demo_func_07_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum = 0
-    for i in range(10):
-        sum += (i+1)*(xx[i]**2)
-    sum += np.random.rand(1)
-    return sum
-def demo_func_07_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum = 0
-    for i in range(30):
-        sum += (i + 1) * (xx[i] ** 2)
-    sum += np.random.rand(1)
-    return sum
-def demo_func_08_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum = 0
-    for i in range(10):
-        sum = sum+(-(xx[i]*math.sin(math.sqrt(abs(xx[i])))))
-    return sum
-def demo_func_08_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum = 0
-    for i in range(30):
-        sum = sum + (-(xx[i] * math.sin(math.sqrt(abs(xx[i])))))
-    return sum
-def demo_func_09_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum = 0
-    for i in range(10):
-        sum += (xx[i]**2-10*math.cos(2*math.pi*xx[i])+10)
-    return sum
-def demo_func_09_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum = 0
-    for i in range(30):
-        sum += (xx[i] ** 2 - 10 * math.cos(2 * math.pi * xx[i]) + 10)
-    return sum
-def demo_func_10_10(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
-    sum_01 = 0
-    sum_02 = 0
-    for i in range(10):
-        sum_01 = sum_01+xx[i]**2
-        sum_02 =sum_02+math.cos(2*math.pi*xx[i])
-    sum_total = -20*math.exp(-0.2*math.sqrt((1/10)*sum_01))-math.exp((1/10)*sum_02)+20+math.e
-    return sum_total
-def demo_func_10_30(x):
-    x1, x2, x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18,x19,x20,x21,x22,x23,x24,x25,x26,x27,x28,x29,x30 = x
-    xx = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24,
-          x25, x26, x27, x28, x29, x30]
-    sum_01 = 0
-    sum_02 = 0
-    for i in range(30):
-        sum_01 = sum_01 + xx[i] ** 2
-        sum_02 = sum_02 + math.cos(2 * math.pi * xx[i])
-    sum_total = -20 * math.exp(-0.2 * math.sqrt((1 / 30) * sum_01)) - math.exp((1 / 30) * sum_02) + 20 + math.e
-    return sum_total
 
 if __name__ == '__main__':
     print(sphere((0, 0)))
